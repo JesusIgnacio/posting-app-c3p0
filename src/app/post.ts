5b81487d7b87989b8b367c8fd43209ee4bcb779e
jcastillo <ignacio.casbar@gmail.com>
2019-11-19 02:04:48 -0300

    add: add css

--- src/app/post.ts
+++ src/app/post.ts
@@ -1,7 +1,7 @@
 export interface Post {
   id_post: string;
   story_id: string;
-  story_tittle: string;
+  story_title: string;
   story_url: string;
   created_at: string;
   story_state: boolean;
